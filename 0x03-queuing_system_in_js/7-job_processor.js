807525a7d08af1e66f15a2d4a305b58b7e3ac597
Lesego Nkosi <nkosilesego8@gmail.com>
2025-06-19 14:51:03 +0000

    Queuing System in JS

---
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,20 @@
+#!/usr/bin/yarn dev
+import { createQueue, Job } from 'kue';
+
+const BLACKLISTED_NUMBERS = ['4153518780', '4153518781'];
+const queue = createQueue();
+
+/**
+ * Sends a push notification to a user.
+ * @param {String} phoneNumber
+ * @param {String} message
+ * @param {Job} job
+ * @param {*} done
+ */
+const sendNotification = (phoneNumber, message, job, done) => {
+  let total = 2, pending = 2;
+  let sendInterval = setInterval(() => {
+    if (total - pending <= total / 2) {
+      job.progress(total - pending, total);
+    }
+
